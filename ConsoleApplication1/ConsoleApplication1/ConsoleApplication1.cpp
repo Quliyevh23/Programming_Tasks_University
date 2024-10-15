58736d228041c9b57836e3cfb72a665c6fe90932
Quliyevh23 <aliquliyev1290@gmail.com>
2024-10-15 07:04:11 +0300

    Project completed

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*double point;
+	cout << "Balinizi daxil edin: ";
+	cin >> point;
+
+	if (point < 0 || point > 100)
+		cout << "Duzgun bal daxil edin.";
+	else if (point < 51)
+		cout << "Fail";
+	else if (point >= 51 && point <= 61)
+		cout << "E (qenaet bexs)";
+	else if (point >= 61.1 && point <= 70)
+		cout << "D (kafi)";
+	else if (point >= 70.1 && point <= 80)
+		cout << "C (yaxsi)";
+	else if (point >= 80.1 && point <= 90)
+		cout << "B (chox yaxsi)";
+	else if (point >= 90.1 && point <= 100)
+		cout << "A (Ela)";*/
+
+
+	/*int a;
+	int b;
+	char Operation;
+	int close = 0;
+
+	while (close == 0) {
+		cout << "\n";
+		cout << "\n Enter the operation:";
+		cin >> Operation;
+		cout << "Enter the numbers:";
+		cin >> a >> b;
+		switch (Operation) {
+		case '0':
+			cout << "Exiting";
+			close++;
+			break;
+		case '+': cout << a << "+" << b << "=" << a + b; break;
+		case 'x': cout << a << "x" << b << "=" << a * b; break;
+		case '-': cout << a << "-" << b << "=" << a - b; break;
+		case '/': cout << a << "/" << b << "=" << a / b; break;
+
+		}
+	}*/
+
+
+
+
+	// FIBONACCI NUMS ---------------------  Faktorial
+
+	/*int num;
+	int newNum = 1;
+	cout << "eded daxil edin: ";
+	cin >> num;
+	for (int i = 1; i <= num; i++) {
+		newNum = newNum * i;
+	}
+	cout << newNum; */
+
+
+	////fibonacci: 0 1 2 3 5 8
+	//int length;
+	//int a = 0;
+	//int b = 1;
+	//int c = a + b;
+	//
+	//cout << "Uzunluq daxil edin: ";
+	//cin >> length;
+	//for (int i = 0; i < length; i++) {
+	//	// a   b   c
+	//	// 0 + 1 = 1
+	//	// 
+	//	// a   b   c
+	//	// 1 + 1 = 2
+	//	// 2 + 1 = 3
+	//	a = b;
+	//	b = c;
+	//	c = a + b;
+	//	cout << c << endl;
+
+	//}
+
+
+}
